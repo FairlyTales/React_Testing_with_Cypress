4c236238459da3eb1665d61ce5d291a8ab1ad9ec
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-09-16 22:13:00 +0300

    simple integration testing

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -21,3 +21,19 @@ describe("unit testing", () => {
     expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
   });
 });
+
+// well, in the real world integration testing is not just about combining
+// several unit tests. It's just one part of it and we will do it here as
+// an example
+describe("integration testing", () => {
+  test("pay button is disabled at first and enabled after the field are filled", async () => {
+    render(<TransactionCreateStepTwo sender={{ id: "2" }} receiver={{ id: "asd" }} />);
+
+    expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+
+    userEvent.type(screen.getByPlaceholderText(/amount/i), "100");
+    userEvent.type(screen.getByPlaceholderText(/add a note/i), "added note");
+
+    expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+  });
+});
